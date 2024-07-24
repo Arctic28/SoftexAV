adbdaee97eb0310d0d027b8e0ff06ad23d8b9636
Pedro Buarque <pedroh.buarque@gmail.com>
2024-07-24 11:48:58 -0300

    teste de primeira lição

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-function medalhaDeAcordoComPosicao(posicao)
+const produtos = [
+  {id: 1, nome: "Camisa" },
+{ id: 2, nome: "Calça"},
+{id: 3, nome: "Sandalias"},
+];
+
+function EncontrarProdutoPorId (id){
+  const  produtoEncontrado = produtos.find((produto) => produto.id === id)
+ return 
+}
+
+  console.log(produtoEncontrado);
+
+EncontrarProdutoPorId((1))
